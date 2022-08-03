70dcc582663e996b393156785d2846551f408d50
Quratul Ain <107280962+QAin-codes@users.noreply.github.com>
2022-08-03 19:33:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Quratul Ain
+#8/07/2022
+#any timestable(up to 12)
+
+print ("\n times table :\n")
+times=int(input("what times table would you like ?" ))
+
+num=1
+for number2 in range (times,times*12,times):
+  print(num,"x",times,"=",number2)
+  num=num+1
